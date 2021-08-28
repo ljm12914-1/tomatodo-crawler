63e6f620f1c41b1ced88d2df66dc326a80398121
ljm12914 <LJM12914@outlook.com>
2021-08-28 16:35:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from PIL import Image
 from io import BytesIO
 import time
 headers = {
-    'User-Agent':'Dalvik/2.1.0(Linux;U;Android8.1.0;RedmiNote8ProMIUI/V11.0.3.0.OEGCNXM)',
-    'Host':'poster.tomatotime.cn',
+    'User-Agent':'Dalvik/2.1.0(Linux;U;Android8.1.0;FQTODO Build/RKQ1.200000.123)',
+    'Host':'poster.fqtodo.cn',
     'Connection':'Keep-Alive',
     'Accept-Encoding':'gzip'
     }
@@ -15,7 +15,7 @@ i = 1
 while(i < 2000):
     print("Try download pic " + str(i))
     try:
-        r = requests.get('http://poster.tomatotime.cn/test' + str(i) + '.JPG', headers=headers)
+        r = requests.get('http://poster.fqtodo.cn/test' + str(i) + '.png', headers=headers)
         image = Image.open(BytesIO(r.content))
     except PIL.UnidentifiedImageError:
         print("Try download pic " + str(i) + " failed.")
@@ -23,6 +23,6 @@ while(i < 2000):
         i += 1
     else:
         print("Try download pic " + str(i) + " succeed")
-        image.save('E:/tomatodo/' + str(i) + '.jpg')
+        image.save('E:/tomatodo/' + str(i) + '.png')
         time.sleep(0.5)
         i += 1
